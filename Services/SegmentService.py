7880b0204b681937800eda661dac2955218dacd4
Klayver-Gabriel <avviolaouclassico@gmail.com>
2025-10-16 12:58:19 -0300

    feat: show multiple segments works automatically, generating random colors and transparency for the meshes

--- Services/SegmentService.py
+++ Services/SegmentService.py
@@ -1,9 +1,20 @@
+import random
+
 from totalsegmentator.python_api import totalsegmentator
 def segmentate(image:str,output_name:str):
-    totalsegmentator(image,output_name,ml=True)
+    totalsegmentator(image,output_name,ml=True,fastest=True)
 
 def segment_map(segments_indeces):
-   #TODO: CRIAR UMA LISTA DE OBJETOS QUE POSSUEM CARACTERISTICA COR e TRANSPARÊNCIA
-   for index in segments_indeces:
-       map = {
-       }
+    segments_properties = {}
+    for i,index in enumerate(segments_indeces):
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        color = (r, g, b)
+        transparency = random.uniform(0.5,0.8)
+        segments_properties[index] = {
+           'color':color,
+           'alpha':transparency,
+        }
+        print(f"Indice: {index} cor: {color} transparência: {transparency}")
+    return segments_properties
--- Services/ShowSegmentsService.py
+++ Services/ShowSegmentsService.py
@@ -2,6 +2,9 @@ import vedo
 import nibabel as nib
 import numpy as np
 
+from Services import SegmentService, ProcessSegmentService
+
+
 def show_segments(input_image):
 
     volume = vedo.Volume(input_image)
@@ -9,33 +12,26 @@ def show_segments(input_image):
     vedo.show(mesh, __doc__, axes=4)
 
 
-def show_multiple_segments(ml_path,segment_map):
+def show_multiple_segments(ml_path,style):
     img_ml = nib.load(ml_path)
     data_ml = img_ml.get_fdata()
-    segment_id = []
     actors = []
     volume = vedo.Volume(data_ml)
-    data = volume.tonumpy()
 
-    unique_labels = np.unique(data)
-
-    for label in unique_labels:
-        if label > 0:
-            segment_id.append(label)
+    segment_id = ProcessSegmentService.process_segment_index(ml_path)
+    segments_properties = SegmentService.segment_map(segment_id)
 
     for segment in segment_id:
+        actors.append(customize_mesh(segment,segments_properties, volume,style))
 
-        if segment not in segment_map:
-            continue
-        actors.append(customize_mesh(segment,segment_map, volume))
+    vedo.show(actors,title="Complete body part image",axes=4,bg='black',size=(1000, 800))
 
-    vedo.show(actors,title="TotalSegmentator Multilabel Visualization",axes=2,bg='black',size=(1000, 800))
-
-def customize_mesh(segment,segment_map,volume):
-
-    segment_alpha, segment_color = segment_map[segment]
+def customize_mesh(segment,segment_map,volume,style):
+    properties = segment_map[segment]
+    segment_color = properties['color']
+    segment_alpha = properties['alpha']
     mesh = volume.isosurface(value=segment)
     mesh.c(segment_color)
-    mesh.alpha(segment_alpha)
-    mesh.lighting('plastic')
+    mesh.alpha(float(segment_alpha))
+    mesh.lighting(style)
     return mesh
